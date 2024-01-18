42684ab510a40dce18ba78b3e5275562d7e402a8
berdal84 <berenger@42borgata.com>
2024-01-18 11:22:41 -0500

    feat(frontend): use Table in Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
+import { Box } from "@mui/material"
+import Button from "./components/Button"
+import Table from "./components/Table"
+import { MOCK_SAMPLES } from "./mocks/samples"
 
 export default function Home() {
 
   return (
-    <>
-      <p>Home page content will be here</p>
-    </>
+    <Box className="flex flex-col gap-5">
+      <Box>
+        <p>Welcome to BioStack, a FastAPI/NextJS exercise</p>
+        <p>Find below the most recent Samples.</p>
+      </Box>
+      <Table rows={MOCK_SAMPLES}></Table>
+      <Button>Click me</Button>
+    </Box>
   )
 }
