5d6df60c4d3c1a81f61c9683521d9e6db25ca660
berdal84 <berenger@42borgata.com>
2024-01-19 17:55:37 -0500

    doc(backend): add documentation with pydantic

--- backend/src/schemas/sample.py
+++ backend/src/schemas/sample.py
@@ -1,36 +1,23 @@
 import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from .page import Page
 from src.utilities.metaclass import Partial
 
 class SampleBase(BaseModel):
     "Base class for all Sample schemas, for internal use only"
-
-    name: str
-    "Name / Label"
-
-    date: datetime.datetime
-    "Date collected"
-
-    type: str
-    "Type of experiment"
+    name: str = Field(default=None, title="Name / Label", max_length=300)
+    date: datetime.datetime = Field(default=None, title="Date collected")
+    type: str = Field(default=None, title="Type of experiment", max_length=300)
 
 
 class Sample(SampleBase):
     "Sample existing in the database"
-
-    id: int
-    "Unique identifier"
-    
-    file_name: str | None
+    id: int = Field(title="Unique identifier")
+    file_name: str | None = Field(default=None, title="Stored filename (with extension), A null value means no file is currently attached to the sample.")
     """
-    Stored filename (with extension).
-
-    A null value means no file is currently attached to the sample.
     Evolution:
         we might have a file table in the future to hide the real name and store the user's file name, and extension
     """
-
 
 SampleCreate = SampleBase # Simple alias
 "Minimal data to create a new Sample"
